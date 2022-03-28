16457ec29461478dc8379df1e0dd2903991bdfa5
ddzik19 <99027550+20095087@users.noreply.github.com>
2022-03-28 23:25:06 +0100

    Updated new files
    I have added necessary code into XMLSerializer.kt
    
    - Added two new functions to NoteAPI.kt, load(), store()
    - updated the NoteAPI class
    - updated the noteAPI variable to Main.kt

--- notes-app/src/main/kotlin/Main.kt
+++ notes-app/src/main/kotlin/Main.kt
@@ -1,14 +1,16 @@
 import controllers.NoteAPI
 import models.Note
 import mu.KotlinLogging
+import persistence.XMLSerializer
 import utils.ScannerInput
 import utils.ScannerInput.readNextInt
 import utils.ScannerInput.readNextLine
+import java.io.File
 import java.lang.System.exit
 
 
 private val logger = KotlinLogging.logger {}
-private val noteAPI = NoteAPI()
+private val noteAPI = NoteAPI(XMLSerializer(File("notes.xml")))
 
 fun main(args: Array<String>) {
     runMenu()
